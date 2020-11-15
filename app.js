e58c3aa2d9b68cdefa9cb67e4b1e8d469897a156
KlausSkovsmose <klaus.skovsmose@gmail.com>
2020-11-15 16:41:05 +0100

    add node js

---
+++ app.js
@@ -0,0 +1,18 @@
+const epxress = require('express')
+const app = express()
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+app.listen(3000)
