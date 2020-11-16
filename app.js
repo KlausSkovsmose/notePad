2c4fe03482e99a32e646ecbcf6bc61ce34330b72
KlausSkovsmose <klaus.skovsmose@gmail.com>
2020-11-16 11:41:28 +0100

    configure node server

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const epxress = require('express')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({ path: './config/config.env'})
+
+connectDB()
+
 const app = express()
 
-const host = 'localhost';
-const port = 8000;
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
+const PORT = process.env.PORT || 3000
 
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
-
-app.listen(3000)
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
