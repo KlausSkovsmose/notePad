c78ff184cefeb92fbe43f9996b40a8e6f8b836e1
KlausSkovsmose <klaus.skovsmose@gmail.com>
2020-11-18 14:48:50 +0100

    Add CRUD to note api

--- routes/notes.js
+++ routes/notes.js
@@ -1,27 +1,61 @@
-const express = require('express')
-const router = express.Router()
-const Note = require('../models/Note')
+const express = require("express");
+const router = express.Router();
+const Note = require("../models/Note");
 
-router.get('/', async (req, res) => {
-    try{
+// Gets a specific note
+router.get("/:noteId", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.noteId);
+    res.json(note);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-    } catch (err){
-        res.json({ message: err })
-    }
-})
+// Gets back all notes
+router.get("/", async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-router.post('/', async (req, res) => {
-    const note = new Note({
-        title: req.body.title,
-        description: req.body.description
-    })
+// Submits a note
+router.post("/", async (req, res) => {
+  const note = new Note({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-    try {
-        const savedNote = await note.save()
-        res.json(savedNote)
-    } catch (err){
-        res.json({ message: err})
-    }
-})
+  try {
+    const savedNote = await note.save();
+    res.json(savedNote);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-module.exports = router
+// Delete a specific note
+router.delete("/:noteId", async (req, res) => {
+  try {
+    const removedNote = await Note.remove({ _id: req.params.noteId });
+    res.json(removedNote);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Update a note
+router.patch("/:noteId", async (req, res) => {
+  try {
+    const updatedNote = await Note.updateOne(
+      { _id: req.params.noteId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedNote)
+  } catch (err) {}
+});
+
+module.exports = router;
