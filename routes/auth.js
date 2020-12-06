1cbb4d8b829e375635f57eb088c6b61131c293c7
KlausSkovsmose <klaus.skovsmose@gmail.com>
2020-12-06 11:52:20 +0100

    Add register and login validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,54 @@
-const router = require('express').Router()
-const User = require('../models/User')
-const { registerValidation } = require('../validation')
+const router = require("express").Router();
+const User = require("../models/User");
+const jwt = require("jasonwebtoken");
+const bcrypt = require("bcryptjs");
+const { registerValidation, loginValidation } = require("../validation");
 
-router.post('/register', async (req, res) => {
-    
-    // Validate data before creating user
-    const {error} = registerValidation(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+router.post("/register", async (req, res) => {
+  // Validate data before creating user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // Create User
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    })
-    try {
-        const savedUser = await user.save()
-        res.send(savedUser)
-    }catch(err){
-        res.status(400).send(err)
-    }
-})
+  // Check if user already is in data base
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
 
-module.exports = router
+  // Hashing password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    // to show all user information replace 'savedUser' with { user: user._id }
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if user already is in data base
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email does not exist");
+
+  // Check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  // Create and assing json web token
+  const token = jwt.sign({_id: user._id})
+
+  res.send("Logged in");
+});
+
+module.exports = router;
