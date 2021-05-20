39acc0f00d899982d65f4dff242b9ca5711be611
Darwish Mammo <darwish.mammo@gmail.com>
2021-05-20 11:16:03 +0200

    implemented routing functionality with react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Showcase from "./components/showcase";
 import { UserProvider } from "./context/user.context";
+import { Switch, Route } from "react-router";
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
         <Navbar />
-        <Showcase />
+        <Switch>
+          <Route exact path="/">
+            <Showcase />
+          </Route>
+          <Route exact path="/dashboard">
+            <h1>This is the dashboard</h1>
+          </Route>
+        </Switch>
       </UserProvider>
     </div>
   );
